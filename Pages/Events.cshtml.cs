9757b8fcf10ab903c548f45f40f9268007b9d457
RowanBattles <rowanvanderweel@gmail.com>
2022-12-22 18:48:47 +0100

    Nieuwe pagina, met google maps

--- Pages/Events.cshtml.cs
+++ Pages/Events.cshtml.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyWebApp.Pages;
 
-public class DownloadModel : PageModel
+public class Eventmodel : PageModel
 {
-    private readonly ILogger<DownloadModel> _logger;
+    private readonly ILogger<Eventmodel> _logger;
 
-    public DownloadModel(ILogger<DownloadModel> logger)
+    public Eventmodel(ILogger<Eventmodel> logger)
     {
         _logger = logger;
     }
@@ -16,4 +16,3 @@ public class DownloadModel : PageModel
     {
     }
 }
-
